206727f109a73eae2d6caac228b278836bb83d37
Zor Rothman <zor.rothman@gmail.com>
2019-11-25 17:23:52 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -8,7 +8,11 @@ def display_rainbow(colors)
   print "G: #{colors[3]}, "
   print "B: #{colors[4]}, "
   print "I: #{colors[5]}, "
+<<<<<<< HEAD
   print "V: #{colors[6]}\n"
+=======
+  print "V: #{colors[6]}"
+>>>>>>> 034619b45b19eb2028b82516db2102e2088e2c6d
 end
 
 print display_rainbow(colors)
